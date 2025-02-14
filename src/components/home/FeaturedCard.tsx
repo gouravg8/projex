005057fdb53dd0a7dc73a3f7f2f0f4d202e9486d
Gourav soni <gouravsoni8476@gmail.com>
2025-02-14 20:55:05 +0530

    Refine FeaturedCard component shadow effects for enhanced visual depth

--- src/components/home/FeaturedCard.tsx
+++ src/components/home/FeaturedCard.tsx
@@ -11,7 +11,7 @@ export default function FeatureCard({
 	description,
 }: FeatureCardProps) {
 	return (
-		<div className="flex flex-col items-center p-6 text-center rounded-lg bg-foreground shadow-[0_0_50px_-12px] shadow-foreground backdrop-blur-xl cursor-pointer hover:-translate-y-4 duration-300">
+		<div className="flex flex-col items-center p-6 text-center rounded-lg bg-foreground shadow-[0_0_30px_-12px] shadow-foreground backdrop-blur-2xl cursor-pointer hover:-translate-y-4 duration-300">
 			<div>
 				{React.cloneElement(icon as React.ReactElement, {
 					className: "size-14 mb-2 text-background",
