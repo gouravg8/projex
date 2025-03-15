58bf1884069e6c71d083597cc3fc9ec326abaa10
Gourav soni <gouravsoni8476@gmail.com>
2025-03-15 12:57:45 +0530

    feat: restructure database schema else it was deleting the project and users

--- prisma/migrations/20250315071319_init/migration.sql
+++ prisma/migrations/20250315071319_init/migration.sql
@@ -1,26 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Project` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Project" DROP CONSTRAINT "Project_authorId_fkey";
-
--- DropTable
-DROP TABLE "Profile";
-
--- DropTable
-DROP TABLE "Project";
-
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "projects" (
     "id" SERIAL NOT NULL,
